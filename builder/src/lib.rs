5cfb1af611364815270baf7504a8b6f3d5811468
Abner Zheng <abnerzheng@gmail.com>
2024-06-16 23:39:50 +0800

    pass builder#4, build

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,4 +1,5 @@
 use proc_macro::TokenStream;
+use proc_macro2::Ident;
 
 use quote::{format_ident, quote};
 use syn::{parse_macro_input, Data, DeriveInput, Fields, FieldsNamed};
@@ -7,8 +8,8 @@ use syn::{parse_macro_input, Data, DeriveInput, Fields, FieldsNamed};
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    let input_ident = &input.ident;
-    let command_builder_ident = format_ident!("{}Builder", input_ident);
+    let command_ident = &input.ident;
+    let command_builder_ident = format_ident!("{}Builder", command_ident);
 
     // eprintln!("{:#?}", input.attrs);
     // eprintln!("{:#?}", input.data);
@@ -29,17 +30,22 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let setters = setter_methods(fields);
     let fields_def = fields_definitions(fields);
     let fields_default_value = fields_default_values(fields);
+    let build_method = build_method(fields, command_ident);
+    eprintln!("{:#?}", build_method.to_string());
 
     let expand = quote! {
+        use std::error::Error;
         pub struct #command_builder_ident {
             #(#fields_def)*
         }
 
         impl #command_builder_ident {
             #(#setters)*
+
+            #build_method
         }
 
-        impl #input_ident{
+        impl #command_ident{
             pub fn builder() -> #command_builder_ident{
                 let builder = #command_builder_ident{
                     #(#fields_default_value)*
@@ -86,11 +92,29 @@ fn setter_methods(fields: &FieldsNamed) -> Vec<proc_macro2::TokenStream> {
             let name = field.ident.as_ref().unwrap();
             let ty = &field.ty;
             quote! {
-                 fn #name(&mut self, #name: #ty) -> &mut Self {
+                 pub fn #name(&mut self, #name: #ty) -> &mut Self {
                     self.#name = Some(#name);
                     self
                  }
             }
         })
         .collect::<Vec<_>>()
+}
+
+fn build_method(fields: &FieldsNamed, command_ident: &Ident) -> proc_macro2::TokenStream {
+    let field_check_and_set = fields.named.iter().map(|field| {
+        let name = field.ident.as_ref().unwrap();
+        let err_msg = format!("field `{}` is missing", name.to_string());
+
+        quote! {
+            #name: self.#name.as_ref().ok_or(#err_msg)?.clone(),
+        }
+    });
+    quote! {
+        pub fn build(&mut self) -> Result<#command_ident, Box<dyn Error>> {
+            Ok(#command_ident{
+                #(#field_check_and_set)*
+            })
+        }
+    }
 }
